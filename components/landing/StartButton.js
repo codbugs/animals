d992fa4b3cd5e54d1c6248f76c29ac652a326f52
Jaime López <jaime.lopez.lopez@live.com>
2021-03-07 16:16:47 +0100

    feat(app): refactor click event on starting button

--- components/landing/StartButton.js
+++ components/landing/StartButton.js
@@ -2,7 +2,12 @@ import styles from './StartButton.module.css';
 
 
 export default function StartButton(props) {
-    return <button type={'button'} className={'button is-primary is-uppercase ' + styles.button} onClick={() => {
-        props.action && props.action()
-    }}>Empezar</button>;
+
+    const clickHandler = () => {
+        if(props.action) {
+            props.action();
+        }
+    };
+
+    return <button type={'button'} className={'button is-primary is-uppercase ' + styles.button} onClick={() => clickHandler()}>Empezar</button>;
 }
