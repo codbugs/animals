18d3af99f49ed02d118a15b4c88dabc198b6c1ad
Jaime López <jaime.lopez.lopez@live.com>
2021-03-07 16:24:39 +0100

    style(app): add css classes to Game component

--- components/game/Game.js
+++ components/game/Game.js
@@ -5,15 +5,18 @@ import Questions from './Questions.js';
 import Solution from './Solution.js';
 import Title from './Title.js';
 
+
+// styles imports
+import styles from './Game.module.css';
+
+
 export default function Game(props) {
 
     const engine = props.engine;
     const [solution, setSolution] = useState(null);
     const [isSolutionActive, setIsSolutionActive] = useState(false);
 
-    return <div className={'colums is-multiline'} style={{
-        marginTop: '50px'
-    }}>
+    return <div className={'colums is-multiline ' + styles.columns}>
         <div className={'column is-full'}>
             <Title />
             <Questions engine={engine} solve={(animal) => {
