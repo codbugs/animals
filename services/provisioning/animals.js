0c539f071e1d64e773525421b2221cdb61c102aa
Jaime López <jaime.lopez.lopez@live.com>
2021-03-14 17:11:02 +0100

    feat(app): add more animals

--- services/provisioning/animals.js
+++ services/provisioning/animals.js
@@ -1,76 +1,144 @@
 export default function AnimalsProvisioning(service) {
     service.create({
-        name: 'Elefante',
-        tipo: 'Mamífero',
-        trompa: 'Sí',
-        habitat: 'Tierra',
-        veneno: 'No',
+        name: 'Elefante', tipo: 'Mamífero', trompa: 'Sí', habitat: 'Tierra', herbivoro: 'Sí',
         imageUrl: '/assets/Elephant_128.png'
     });
     
     service.create({
-        name: 'Jirafa',
-        tipo: 'Mamífero',
-        cuello_largo: 'Sí',
-        domestico: 'No',
-        habitat: 'Tierra',
-        veneno: 'No',
+        name: 'Jirafa', tipo: 'Mamífero', cuello_largo: 'Sí', habitat: 'Tierra', herbivoro: 'Sí',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+    
+    service.create({
+        name: 'León', tipo: 'Mamífero', carnivoro: 'Sí', melena: 'Sí', felino: 'Sí', habitat: 'Tierra',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+    
+    service.create({
+        name: 'Oso', tipo: 'Mamífero', omnivoro: 'Sí', carnivoro: 'Sí', herbivoro: 'Sí', habitat: 'Tierra',
         imageUrl: '/assets/Elephant_128.png'
     });
 
     service.create({
-        name: 'Perro',
-        tipo: 'Mamífero',
-        domestico: 'Sí',
-        habitat: 'Tierra',
-        veneno: 'No',
+        name: 'Perro', tipo: 'Mamífero', carnivoro: 'Sí', domestico: 'Sí', canino: 'Sí', habitat: 'Casa',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+
+    service.create({
+        name: 'Gato', tipo: 'Mamífero', carnivoro: 'Sí', domestico: 'Sí', habitat: 'Casa', felino: 'Sí',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+
+    service.create({
+        name: 'Conejo', tipo: 'Mamífero', herbivoro: 'Sí', domestico: 'Sí', habitat: 'Granja', orejas_alargadas: 'Sí',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+
+/*
+    service.create({
+        name: 'Caballo',tipo: 'Mamífero', herbivoro: 'Sí', domestico: 'Sí', habitat: 'Granja',
+        imageUrl: '/assets/Elephant_128.png'
+    });    
+*/
+    service.create({
+        name: 'Gallina', tipo: 'Ave', omnivoro: 'Sí', carnivoro: 'Sí', herbivoro: 'Sí', domestico: 'Sí', habitat: 'Granja',
+        imageUrl: '/assets/Elephant_128.png'
+    });  
+
+    service.create({
+        name: 'Cerdo', tipo: 'Mamífero', omnivoro: 'Sí', carnivoro: 'Sí', herbivoro: 'Sí', domestico: 'Sí', habitat: 'Granja', cola_tirabuzon: 'Sí',
         imageUrl: '/assets/Elephant_128.png'
     });
     
     service.create({
-        name: 'Pingüino',
-        tipo: 'Ave',
-        domestico: 'No',
-        habitat: 'Tierra',
-        veneno: 'No',
+        name: 'Pingüino', tipo: 'Ave', carnivoro: 'Sí', habitat: 'Tierra',
         imageUrl: '/assets/Elephant_128.png'
     });
     
     service.create({
-        name: 'Tiburón',
-        tipo: 'Pez',
-        domestico: 'No',
-        habitat: 'Agua',
-        veneno: 'No',
+        name: 'Cigüeña', tipo: 'Ave', omnivoro: 'Sí', carnivoro: 'Sí', herbivoro: 'Sí', habitat: 'Aire',
         imageUrl: '/assets/Elephant_128.png'
     });
     
     service.create({
-        name: 'Ornitorrinco',
-        tipo: 'Mamífero',
-        domestico: 'No',
-        habitat: 'Tierra',
-        veneno: 'Sí',
-        tipoPico: 'Pato',
+        name: 'Águila', tipo: 'Ave', carnivoro: 'Sí', habitat: 'Aire',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+    
+    service.create({
+        name: 'Canario',tipo: 'Ave', domestico: 'Sí', comida: 'Herbívoro', habitat: 'Aire',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+    
+    service.create({
+        name: 'Tiburón', tipo: 'Pez', habitat: 'Agua',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+
+    service.create({
+        name: 'Pez Espada', tipo: 'Pez', habitat: 'Agua', nariz_espada: 'Sí',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+
+    service.create({
+        name: 'Calamar', tipo: 'Molusco', habitat: 'Agua', tentaculos: 'Sí',
+        imageUrl: '/assets/Elephant_128.png'
+    });
+    
+    service.create({
+        name: 'Ornitorrinco', tipo: 'Mamífero', habitat: 'Tierra', veneno: 'Sí', tipoPico: 'Pato',
         imageUrl: '/assets/Platypus_128.png'
     });
     
     service.create({
-        name: 'Camaleón',
-        tipo: 'Reptil',
-        domestico: 'No',
-        habitat: 'Tierra',
-        cambiaColor: 'Sí',
-        veneno: 'No',
+        name: 'Camaleón', tipo: 'Reptil', habitat: 'Tierra', cambiaColor: 'Sí',
         imageUrl: '/assets/Chamaleon_128.png'
     });
     
     service.create({
-        name: 'Cobra',
+        name: 'Cobra', tipo: 'Reptil', habitat: 'Tierra', veneno: 'Sí',
+        imageUrl: '/assets/Chamaleon_128.png'
+    });
+    
+    service.create({
+        name: 'Tortuga',
         tipo: 'Reptil',
         domestico: 'No',
         habitat: 'Tierra',
-        veneno: 'Sí',
+        caparazon: 'Sí',
+        imageUrl: '/assets/Chamaleon_128.png'
+    });
+    
+    service.create({
+        name: 'Araña',
+        tipo: 'Insecto',
+        habitat: 'Tierra',
+        telaraña: 'Sí',
+        imageUrl: '/assets/Chamaleon_128.png'
+    });
+    
+    service.create({
+        name: 'Abeja',
+        tipo: 'Insecto',
+        habitat: 'Aire',
+        vuela: 'Sí',
+        miel: 'Sí',
+        imageUrl: '/assets/Chamaleon_128.png'
+    });
+    
+    service.create({
+        name: 'Mariposa',
+        tipo: 'Insecto',
+        habitat: 'Aire',
+        vuela: 'Sí',
+        imageUrl: '/assets/Chamaleon_128.png'
+    });
+    
+    service.create({
+        name: 'Caracol',
+        tipo: 'Molusco',
+        habitat: 'Tierra',
+        caparazon: 'Sí',
         imageUrl: '/assets/Chamaleon_128.png'
     });
 }
--- services/provisioning/questions.js
+++ services/provisioning/questions.js
@@ -42,7 +42,7 @@ export default function QuestionsProvisioning(engine) {
       engine.create({
         key: 'tipo',
         question: '¿Qué tipo de animal has pensado?',
-        answers: ['Mamífero', 'Ave', 'Pez', 'Insecto', 'Reptil'],
+        answers: ['Mamífero', 'Ave', 'Pez', 'Insecto', 'Reptil', 'Molusco'],
         filter: (animal, answer) => {
           return animal['tipo'] === answer;
         }
@@ -51,7 +51,7 @@ export default function QuestionsProvisioning(engine) {
       engine.create({
         key: 'habitat',
         question: '¿Dónde vive?',
-        answers: ['Tierra', 'Aire', 'Agua'],
+        answers: ['Tierra', 'Aire', 'Agua', 'Granja'],
         filter: (animal, answer) => {
           return animal['habitat'] === answer;
         }
@@ -81,6 +81,132 @@ export default function QuestionsProvisioning(engine) {
         answers: ['Pato', 'Otro'],
         filter: (animal, answer) => {
           return animal['tipoPico'] === answer || Object.keys(animal).includes('tipoPico') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'tentaculos',
+        question: 'Tiene tentáculos?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['tentaculos'] === answer || Object.keys(animal).includes('tentaculos') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'vuela',
+        question: '¿Vuela?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['vuela'] === answer || Object.keys(animal).includes('vuela') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'miel',
+        question: '¿Hace miel?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['miel'] === answer || Object.keys(animal).includes('miel') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'telaraña',
+        question: '¿Hace telarañas?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['telaraña'] === answer || Object.keys(animal).includes('telaraña') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'caparazon',
+        question: '¿Su casa es un caparazón?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['caparazon'] === answer || Object.keys(animal).includes('caparazon') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'melena',
+        question: '¿Tiene melena?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['melena'] === answer || Object.keys(animal).includes('melena') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'nariz_espada',
+        question: '¿Su nariz tiene forma de espada?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['nariz_espada'] === answer || Object.keys(animal).includes('nariz_espada') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'cola_tirabuzon',
+        question: '¿Su cola tiene forma de tirabuzón?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['cola_tirabuzon'] === answer || Object.keys(animal).includes('cola_tirabuzon') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'orejas_alargadas',
+        question: '¿Sus orejas son alargadas?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['orejas_alargadas'] === answer || Object.keys(animal).includes('orejas_alargadas') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'carnivoro',
+        question: '¿Come carne?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['carnivoro'] === answer || Object.keys(animal).includes('carnivoro') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'herbivoro',
+        question: '¿Come hojas, plantas o semillas?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['herbivoro'] === answer || Object.keys(animal).includes('herbivoro') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'omnivoro',
+        question: '¿Come carne y hojas, plantas o semillas?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['omnivoro'] === answer || Object.keys(animal).includes('omnivoro') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'felino',
+        question: '¿Pertenece a la familia de los felinos?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['felino'] === answer || Object.keys(animal).includes('felino') === false;
+        }
+      });
+    
+      engine.create({
+        key: 'canino',
+        question: '¿Pertenece a la familia de los caninos?',
+        answers: ['Sí', 'No'],
+        filter: (animal, answer) => {
+          return animal['canino'] === answer || Object.keys(animal).includes('canino') === false;
         }
       });
 }
